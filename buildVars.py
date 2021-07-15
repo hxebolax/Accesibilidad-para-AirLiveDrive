ae19f4fc54fcd1b943ac33aee786a20824fbfeb7
Gerardo Késsler <gera.kessler@gmail.com>
2021-07-15 07:36:33 -0300

    corrección de nombre

--- buildVars.py
+++ buildVars.py
@@ -20,7 +20,7 @@ addon_info = {
 	# Add-on summary, usually the user visible name of the addon.
 	# Translators: Summary for this add-on
 	# to be shown on installation and add-on information found in Add-ons Manager.
-	"addon_summary": _("AirAccesibility"),
+	"addon_summary": _("AirAccessibility"),
 	# Add-on description
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description": _("""Hace más accesibles los productos AirExplorer y AirLiveDrive para NVDA"""),
